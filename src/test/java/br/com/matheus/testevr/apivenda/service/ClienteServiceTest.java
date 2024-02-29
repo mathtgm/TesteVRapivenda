63635b6e254bfe1d17e274840f9c1f60b5e7c821
Matheus Gabriel <matheusg663@gmail.com>
2024-02-29 11:34:43 -0300

    Adicionado Mock de validação no teste

--- src/test/java/br/com/matheus/testevr/apivenda/service/ClienteServiceTest.java
+++ src/test/java/br/com/matheus/testevr/apivenda/service/ClienteServiceTest.java
@@ -2,12 +2,15 @@ package br.com.matheus.testevr.apivenda.service;
 
 import br.com.matheus.testevr.apivenda.model.Cliente;
 import br.com.matheus.testevr.apivenda.repository.ClienteRepository;
+import br.com.matheus.testevr.apivenda.validation.cliente.validacaoCadastro.ValidacaoCadastroCliente;
+import br.com.matheus.testevr.apivenda.validation.cliente.validacaoDados.ValidacaoDadosCliente;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,6 +23,12 @@ class ClienteServiceTest {
 
     @Mock
     ClienteRepository clienteRepository;
+
+    @Mock
+    List<ValidacaoCadastroCliente> validacaoCadastroClienteList;
+
+    @Mock
+    List<ValidacaoDadosCliente> validacaoDadosClientesList;
 
     @Captor
     ArgumentCaptor<Cliente> clienteArgumentCaptor;
